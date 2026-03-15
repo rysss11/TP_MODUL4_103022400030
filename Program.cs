a11a68ce64f06cc24001e280fb3ae6e50c45ea2a
Raissa <talanipanabilah@gmail.com>
2026-03-15 23:29:02 +0800

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public enum Kelurahan
+{
+    Batununggal, Kujangsari, Mengger, Wates, Cijaura, 
+    Jatisari, Margasari, Sekejati, Kebonwaru, Maleer
+}
+
+public class KodePos
+{
+    public static int getKodePos(Kelurahan kelurahan)
+    {
+        int[] tableKodePos = {40266, 40287, 40267, 40256, 40287,
+            40286, 40286, 40286, 40272, 40274};
+
+        return tableKodePos[(int)kelurahan]; 
+    }
+}
